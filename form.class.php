52c2b8076f00c95f9e6b2fb5d325516eb7506ef5
Luke Watts <luke@luke-watts.com>
2014-08-15 13:33:17 +0100

    Add necessary CSRF methods and open and close methods

--- form.class.php
+++ form.class.php
@@ -3,29 +3,92 @@
 require_once( 'vendor/NoCSRF/nocsrf.php' );
 
 class Form {
+
+  /**
+  * The CSRF token used by the form builder.
+  *
+  * @var string
+  */
+  protected $csrfToken;
   
-  public function open() {
+  /**
+  * Open up a new HTML form.
+  *
+  * @param  string $name   Name value
+  * @param  string $method Method value. Default: post
+  * @param  string $action Action value
+  * @return string         Returns HTML output
+  */
+  public function open( $name = '', $method = 'post', $action = '' ) {
     
+    $action_name = preg_replace( '.php', '', $action );
+
+    $output = '<form name="' . $name . '" method="' . $method . '" action="' . $action_name . '.php">' . "\n";
+    $output .= $this->hidden();
+
+    return $output;
+
   }
 
+  /**
+   * Close the current form.
+   *
+   * @return string Returns HTML output
+   */
   public function close() {
+    
+    return '</form>';
 
   }
 
-  public function token() {
+
+  public function token( $name = '_token', $value = '' ) {
+    
+    $this->csrfToken = NoCSRF::generate( '_token',  );
+
+    if ( !isset( $value ) || $value == '' ) {
+      $value = $this->csrfToken;
+    }
+    
+    return $this->hidden( $name, $value );
 
   }
 
-  public function hidden() {
-
+  /**
+   * Create a hidden input field.
+   *
+   * @param  string $name  Name of field
+   * @param  string $value Value of field
+   * @return string Return HTML output
+   */
+  public function hidden( $name, $value = null)
+  {
+    return $this->input('hidden', $name, $value);
   }
 
   public function label() {
 
   }
 
-  public function input() {
+  /**
+   * Create a form input field.
+   *
+   * @param string $type
+   * @param string $name
+   * @param string $value
+   * @param array $options
+   * @return string
+   */
+  public function input( $type, $name, $value = null, $id = null, $class = null ) {
+    $attr = array();
 
+   if ( isset( $value ) ) ? $attr['value'] = ' value=' . $value : $attr['value'] = '';
+
+   if ( isset( $id ) ) ? $attr['id'] = ' id=' . $id : $attr['id'] = '';
+
+   if ( isset( $class ) ) ? $attr['class'] = ' class=' . $class : $attr['class'] = '';
+
+    return '<input type="' . $type . '" name="' . $name . '"' . $attr['value'] . $attr['id'] . $attr['class'] . '>';
   }
 
   public function text() {
@@ -53,14 +116,6 @@ class Form {
   }
  
   public function button() {
-  
-  }
-
-  protected function getMethod() {
-  
-  }
-
-  protected function getAction() {
   
   }
 
