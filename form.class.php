09bd9c74d321baa551fb030dc3e72bda365aaa17
Luke Watts <luke@luke-watts.com>
2014-08-15 11:57:47 +0100

    Add Form helper class

---
+++ form.class.php
@@ -0,0 +1,65 @@
+<?php
+
+class Form {
+  
+  public function open() {
+    
+  }
+
+  public function close() {
+
+  }
+
+  public function token() {
+
+  }
+
+  public function hidden() {
+
+  }
+
+  public function label() {
+
+  }
+
+  public function input() {
+
+  }
+
+  public function text() {
+
+  }
+
+  public function email() {
+
+  }
+
+  public function textarea() {
+
+  }
+
+  protected function setTextAreaSize() {
+
+  }
+
+  protected function setQuickTextAreaSize() {
+
+  }
+
+  public function submit() {
+ 
+  }
+ 
+  public function button() {
+  
+  }
+
+  protected function getMethod() {
+  
+  }
+
+  protected function getAction() {
+  
+  }
+
+}
